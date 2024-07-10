f74c649a197bb898986a68dd9308f734619af795
BigMoistLochu <krok.konrad1@gmail.com>
2024-07-10 21:23:53 +0200

    added project to repo

---
+++ src/main/java/app/zerotoexpertjavaproject/ZeroToExpertJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package app.zerotoexpertjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroToExpertJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroToExpertJavaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/zerotoexpertjavaproject/ZeroToExpertJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package app.zerotoexpertjavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroToExpertJavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
