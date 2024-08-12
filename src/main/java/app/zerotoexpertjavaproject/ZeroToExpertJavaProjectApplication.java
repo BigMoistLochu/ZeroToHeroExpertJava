af42e9804de3cf641408f6d2f26f2d32d65370c3
s24717 <s24717@pjwstk.edu.pl>
2024-08-13 00:20:18 +0200

    refactored code

--- src/main/java/app/zerotoexpertjavaproject/ZeroToExpertJavaProjectApplication.java
+++ src/main/java/app/zerotoexpertjavaproject/ZeroToExpertJavaProjectApplication.java
@@ -13,9 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZeroToExpertJavaProjectApplication implements CommandLineRunner {
 
 
-    private UserRepository userRepository;
-
-
     public static void main(String[] args) {
         SpringApplication.run(ZeroToExpertJavaProjectApplication.class, args);
     }
